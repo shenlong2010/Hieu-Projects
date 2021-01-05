b9ab3696f7a2ace1ae3649be3b0084062a631850
Loc Lam <shenlong3010@gmail.com>
2021-01-04 20:55:52 -0500

    added jpg

--- findImages.py
+++ findImages.py
@@ -2,7 +2,7 @@
 import cv2 as cv
 import sys
 
-img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+img = cv.imread(cv.samples.findFile("The-Office.jpg"))
 
 if img is None:
     sys.exit("Could not read the image")
@@ -11,5 +11,5 @@ cv.imshow("Display window", img)
 k = cv.waitKey(0)
 
 if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+    cv.imwrite("The-Office.png", img)
 
